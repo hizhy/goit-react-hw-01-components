df0573413f3b6903c0397c9d354cf932b95f879a
Mykola_Kh <hizhym@gmail.com>
2022-12-16 23:37:30 +0200

    1st task markup

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,37 +1,43 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-export const Profile = ({}) => {
+export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div class="profile">
-  <div class="description">
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-      class="avatar"
-    />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
-  </div>
+    <div>
+      <div>
+        <img src={avatar} alt="User avatar" />
+        <p>{username}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
 
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
-    </li>
-  </ul>
-</div>
-  )
-}
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 // export default (Profile)
 
-
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
